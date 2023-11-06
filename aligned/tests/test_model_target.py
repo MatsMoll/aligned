6a5fd2b87249501f857adc08cadd29696d7d6958
Mats E. Mollestad <mast@mollestad.no>
2023-11-06 22:28:35 +0100

    fix: flaky test

--- aligned/tests/test_model_target.py
+++ aligned/tests/test_model_target.py
@@ -124,7 +124,7 @@ async def test_model_insert_predictions() -> None:
 
     await store.insert_into(FeatureLocation.model('test_model'), {'id': [1, 2, 3], 'a': [10, 14, 20]})
 
-    stored_data = pl.read_parquet(path)
+    stored_data = pl.read_parquet(path).select(expected_frame.columns)
     assert stored_data.frame_equal(expected_frame)
 
 
