fedb9f9cd5cef7fe3666c16bd0035e34eaf24c8c
MatsMoll <MatsMoll@users.noreply.github.com>
2024-11-25 22:52:52 +0000

    Generate API documentation 5053735e40ec88988d49166a28d131635d8c64a2

--- storage.py
+++
@@ -1,6 +0,0 @@
-class Storage:
-    async def read(self, path: str) -> bytes:
-        raise NotImplementedError()
-
-    async def write(self, path: str, content: bytes) -> None:
-        raise NotImplementedError()
